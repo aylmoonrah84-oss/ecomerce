416a4798c4876903c3ae2bf79009301d1e2858f1
AylinRah <aylmoonrah84@gmail.com>
2026-05-09 21:51:36 -1200

    Complete first step of logging in
    pure react code of first step form login or reigester

--- src/Pages/Auth/FirstStep/index.jsx
+++ src/Pages/Auth/FirstStep/index.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import fetchData from '../../../Utils/fetchData'
+import notify from '../../../Utils/notify';
 
-export default function FirstStep() {
+export default function FirstStep({phoneNumber,changePhone,changePage}) {
+    const[loading,setIsLoading]=useState('false');
+    const handelSubmit=async(e)=>{
+        e.preventDefult()
+        setIsLoading(true);
+        const result= await fetchData("auth",{
+            method:"POST",
+            headers:{
+                "content-type":"application/json",
+            },
+            body: JSON.stringify(phoneNumber),
+        });
+        notify(result.success?"success":"error",result.massage);
+        if(result.success){
+            changePage(result.data.passwordExist ? "password":"otp");
+        }
+        setIsLoading(false);
+    }
   return (
-    <div>
-      
-    </div>
-  )
+    <form  onSubmit={handelSubmit}className='text-right'>
+      <h2>ورود یا ثبت نام</h2>
+      <input type="text"
+       placeholder='شماره خود را وارد کنید' 
+       name=''
+       value={phoneNumber}
+       onChange={(e)=>changePhone(e.target.value)}
+       id=''
+       />
+      <button disabled={loading||!phoneNumber} type='submit'>ورود</button>
+      <span onClick={()=>changePage('forget')}>فراموشی رمز</span>
+    </form>
+  );
 }
