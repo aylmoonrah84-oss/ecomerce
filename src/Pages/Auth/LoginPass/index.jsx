f0970db617eceb2d427a272188071c61bb7ea25d
AylinRah <aylmoonrah84@gmail.com>
2026-05-11 15:34:44 -1200

    complete loginpass
    the client can login with entering his/her phone number with their own password or with otp code, otherwise they should go to the forget password page.

--- src/Pages/Auth/LoginPass/index.jsx
+++ src/Pages/Auth/LoginPass/index.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import fetchData from '../../../Utils/fetchData';
+import notify from '../../../Utils/notify';
 
-export default function LoginPass() {
+export default function LoginPass({ phoneNumber, ChangePage }) {
+  const [password, setPassword] = useState("");
+  const [loading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  const handeleOtp = async () => {
+    setIsLoading(true);
+    const result = await fetchData("auth/resend-code", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ phoneNumber }),
+    });
+    notify(result.success ? "success" : "error", result.message);
+    if (result.success) ChangePage("otp");
+    setIsLoading(false);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const result = await fetchData("auth/login-password", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ phoneNumber, password }),
+    });
+
+    notify(result.success ? "success" : "error", result.message);
+
+    if (result.success) dispatch(login(result.data));
+
+    setLoading(false);
+  };
   return (
-    <div>
-      
-    </div>
-  )
+    <form onSubmit={handleSubmit} className='text-right'>
+
+      <span onClick={() => ChangePage("first")}>برگشت</span>
+
+      <input type='text'
+        value={phoneNumber}
+        readOnly
+        className='' />
+
+      <input type="password"
+        value={password} onChange={(e) => setPassword(e.target.value)}
+        placeholder='رمز خود را وارد کنید'
+        className='' />
+
+      <button
+        disabled={loading || !phoneNumber || !password}
+        type='submit'
+        className=''>
+        ورود
+      </button>
+      <span onClick={handeleOtp}>  ورود با پیامک</span>
+      <span onClick={() => ChangePage("forget")}>فراموشی رمز؟</span>
+    </form>
+  );
 }
