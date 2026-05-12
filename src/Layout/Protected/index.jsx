ee1793572d2507e04d864bc81af4bf92f9517517
AylinRah <aylmoonrah84@gmail.com>
2026-05-11 14:16:18 -1200

    complete layout

--- src/Layout/Protected/index.jsx
+++ src/Layout/Protected/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet } from 'react-router-dom'
 
 export default function Protected() {
+    const {token}=useSelector(state=>state.auth)
+    if(!token){
+        return <Navigate to={'/auth'}/>
+    }
   return (
-    <div>
-      
-    </div>
+    <><Outlet/></>
   )
 }
--- src/Layout/UnProtected/index.jsx
+++ src/Layout/UnProtected/index.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
 
 export default function UnProtected() {
+  const { token } = useSelector((state) => state.auth);
+  if (token) {
+    return <Navigate to={"/profile"} />;
+  }
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Outlet />
+    </>
+  );
 }
