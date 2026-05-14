9e1f3ded1578a087b6b415b70a1e7b1013c891a1
AylinRah <aylmoonrah84@gmail.com>
2026-05-13 19:16:59 -1200

    complete router

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../Layout";
+import Home from "../Pages/Home";
+import Protected from "../Layout/Protected";
+import Cart from "../Pages/Cart";
+import Profile from "../Pages/Profile";
+import UnProtected from "../Layout/UnProtected";
+import Auth from "../Pages/Auth";
+import Products from "../Pages/Products";
+import ProductDetails from "../Pages/ProductDetails/Index";
+import About from "../Pages/About/indedx";
 
-export default function Router() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [{
+      index: true,
+      element : <Home/>
+    },
+    {
+      element: <Protected/>,
+      children:[{
+        path:'/card',
+        element:<Cart/>
+      },
+      {
+        path:'/profile',
+        element: <Profile/>
+      }
+    ]
+    },
+    {
+      element:<UnProtected/>,
+      children:[{
+        path:'/auth',
+        element:<Auth/>
+      },
+    ],
+    },
+    {
+      path:'products/:categoryId/:categoryName',
+      element:<Products/>
+    },
+    {
+      path:'product-details/:id/:name',
+      element:<ProductDetails/>
+    },{
+      path:'about',
+      element:<About/>,
+    }
+    ],
+  },
+]);
+export default router
