ef9a5121a7ed6f06ac4b86380db88262272bb834
AylinRah <aylmoonrah84@gmail.com>
2026-06-04 19:22:25 +0330

    base

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,9 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 // import auth from './Slices/AuthSlice'
 import authSlice from './Slices/AuthSlice'
+import cartSlice from './Slices/CartSlice'
+
 const store=configureStore({
     reducer:{
         auth:authSlice,
+        cart:cartSlice,
     }
 })
 export default store
