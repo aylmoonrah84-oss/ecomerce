ef51933815b3a52dbd0aa11dc2a52a9a1133e3ae
AylinRah <aylmoonrah84@gmail.com>
2026-06-04 19:49:29 +0330

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Toaster } from 'react-hot-toast'
 import { RouterProvider } from 'react-router-dom'
 import router from './Router'
-import FirstStep from './Pages/Auth/FirstStep'
+
 
 export default function App() {
   return (
